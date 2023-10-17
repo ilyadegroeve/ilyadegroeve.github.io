3d1aabb7fee41d535e58472503c3b7c7b2aff358
ilyadegroeve <degroeveilya@gmail.com>
2023-10-17 20:32:23 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -11,16 +11,16 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // Set the appropriate image source based on dark mode state
     if (isDarkMode) {
-      logo.src = "C:\\Users\\degro\\Documents\\Site\\index\\images\\main-light.png";
-      themeToggle.src = "C:\\Users\\degro\\Documents\\Site\\index\\images\\darkmode-light.png";
+      logo.src = "images\main-light.png";
+      themeToggle.src = "images\darkmode-light.png";
       
       // Invert social icons colors for dark mode
       socialIcons.forEach(icon => {
         icon.style.filter = "invert(1)";
       });
     } else {
-      logo.src = "C:\\Users\\degro\\Documents\\Site\\index\\images\\main.png";
-      themeToggle.src = "C:\\Users\\degro\\Documents\\Site\\index\\images\\darkmode.png";
+      logo.src = "images\main.png";
+      themeToggle.src = "images\darkmode.png";
       
       // Reset social icons colors for light mode
       socialIcons.forEach(icon => {
